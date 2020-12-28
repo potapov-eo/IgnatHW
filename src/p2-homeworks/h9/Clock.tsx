cbc9e9a46296dcb7a9f669758579eb5e8b449282
potapov-eo <potapov.eo@yandex.ru>
2020-12-28 22:10:29 +0300

    task 9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,49 +1,49 @@
 import React, {useState} from "react";
-import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0);
-    const [date, setDate] = useState<Date>();
+    const [date, setDate] = useState<Date>(new Date());
     const [show, setShow] = useState<boolean>(false);
-
+    const get2digitString = (num:number) => num < 10 ? "0" + num : num
     const stop = () => {
-        // stop
+        clearInterval(timerId,)
     }
     const start = () => {
-        stop();
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000);
         setTimerId(id);
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     };
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     };
 
-    const stringTime = "Time"; // fix with date
+  /*  const stringTime = "Time"; // fix with date
     const stringDate = "Date"; // fix with date
-
+*/
     return (
         <div>
             <div
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
-                {stringTime}
+                TIME: {get2digitString(date?.getHours()) + ":"
+            + get2digitString(date?.getMinutes()) + ":"
+            + get2digitString(date?.getSeconds())}
             </div>
 
             {show && (
                 <div>
-                    {stringDate}
+                    DATE: {date?.toDateString()}
                 </div>
             )}
 
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
+            <button onClick={start}>start</button>
+            <button onClick={stop}>stop</button>
 
         </div>
     );
